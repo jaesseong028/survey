e078dd78cc3696cb08f02de515def623ba3a45fa
jaesung <jaesung@ubcare.co.kr>
2018-01-24 14:21:00 +0900

    test commit3

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,8 @@ namespace UBSurvey
             services.AddMvc();
         }
 
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
