ec2b5bb51ad0ab61fad998e7ddb0dc38f4551b03
allyus <allyus@ubcare.co.kr>
2018-01-25 12:37:55 +0900

    설문 에디터에 필요한 vue.js 및 javascript 추가 및 컨트롤러 추가

---
+++ wwwroot/js/vue-component.js
@@ -0,0 +1,43 @@
+﻿Vue.component('radio-com', {
+    template: '<div>\
+                        <div>{{el.title}}</div>\
+                        <button v-on:click="item_add(el)">답변추가</button> \
+                        <button v-on:click="edit">편집</button> \
+                        <div v-for="(s, index) in el.choices"> \
+                                <input type="radio" :name="el.name" :id="el.name + index" :value="s.item" /> <label :for="el.name + index" v-show="!isEdit">{{s.item}}</label> <input  v-model="s.item" v-show="isEdit">\
+                        </div>\
+                      </div>',
+    data: function () { return { isEdit: false } },
+    props: { el: { type: Object } },
+    methods :{
+        item_add: function (el) {
+            el.choices.push({ item: "item"});
+        }, 
+        edit: function () {
+            console.log(this.isEdit);
+            this.isEdit = !this.isEdit;
+        }
+    }
+})
+
+Vue.component('check-com', {
+    template: '<div>\
+                            <div>{{el.title}}</div>\
+                            <div v-for="(s, index) in el.choices">\
+                                <input type="checkbox" :name="el.name" :value="s.item" :id="el.name + index" />\
+                                <label :for="el.name + index">{{s.item}}</label>\
+                            </div>\
+                     </div>',
+    props: { el: { type: Object } }
+})
+
+
+Vue.component('textarea-com', {
+    template: '<div><div>{{el.title}}</div><textarea  type="text" :name="el.name" :rows="el.rows" style="width:100%"></textarea></div>',
+    props: { el: { type: Object } }
+})
+
+Vue.component('text-com', {
+    template: '<div><div>{{el.title}}</div> 답변 : <input  type="text" :name="el.name" style="width:100%" /></div>',
+    props: { el: { type: Object } }
+})
