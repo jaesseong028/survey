1d572e2446848eff05526ba64c744d10abd13b11
jaesung <jaesung@ubcare.co.kr>
2018-02-12 14:53:36 +0900

    view2 modify

--- wwwroot/js/Example/view2.js
+++ wwwroot/js/Example/view2.js
@@ -19,12 +19,19 @@ var cdom = {
     element: null,
 
     get: function (o){
-        var obj = Object.create(this);
+        var obj = Object.create(this);        
         obj.element = (typeof o == 'object') ? o : document.createElement(o);
         return obj;
     },
+    getcss: function(o,c,i){
+        var obj = cdom.get(o);
+        var objcss = Object.create(this);
+        objcss.element =  obj.element.getElementsByClassName(c)[i];
+        return objcss;
+    },
     append: function(o){
         var obj = cdom.get(o);
+        console.log(1234,obj);
         this.element.appendChild(obj.element);
         return obj;
     },
@@ -51,12 +58,24 @@ var cdom = {
 
         return this;
     },
-    insertA: function(o){
+    insert: function(o){
         var obj = cdom.get(o);
         pelement = this.element.parentNode;
         pelement.insertBefore(obj.element,this.element.nextSibling);
         return obj;
     }
+}
+
+
+function createSurvey()
+{
+    var elSurvey = cdom.get('div').css('survey');
+    elSurvey.append('div')
+                .css('header')
+                .append('p').css('title');
+    elSurvey.append('div').css('page-set');
+
+    return elSurvey.element;
 }
 
 // ////////////////////////////////////
@@ -69,21 +88,16 @@ var cdom = {
 //     </div>
 // </div>
 /////////////////////////////////////// 
-function createPageSet()
-{
-    
-}
-
 function createPage()
 {
-    var elPSet = cdom.get('div').css('page');
-    elPSet.append('div')
+    var elPage = cdom.get('div').css('page');
+    elPage.append('div')
             .css('header')
             .append('p')
                 .css('titile')
-            .insertB('p')
+            .insert('p')
                 .css('description');
-    return elPSet.element;
+    return elPage.element;
 }
 // ////////////////////////////////////
 // /// Question Set 생성
@@ -105,7 +119,7 @@ function createQuestionSet()
             .css('header')
             .append('p')
                 .css('title')
-            .insertA('p')
+            .insert('p')
                 .css('description');
 
     elQSet.append('div')
@@ -124,15 +138,69 @@ function createQuestionItems()
 function domReady (){
     try
     {
+        var surveyId = document.getElementById('ubSurvey');
         var surveyJson = JSON.parse(survey);
-
         console.log(surveyJson);
 
-        var surveyId = document.getElementById('ubSurvey');
+        if( ('survey' in surveyJson) && ('pages' in surveyJson.survey) && ('title' in surveyJson.survey))
+        {
+            //Create Survey 
+            var elSurvey = createSurvey();
+
+            //Survey Title
+            cdom.getcss(elSurvey,'title',0).text(surveyJson.survey.title);
+            
+            //SUrvey Page
+            var pages  = surveyJson.survey.pages;
+
+            for(var page in pages)
+            {
+                if(pages.hasOwnProperty(page))
+                {
+                    //pages[page].element;
+                    var elPage = createPage();
+                    
 
 
 
-        createQuestionSet();
+
+                }
+            }
+
+
+
+
+            surveyId.appendChild(elSurvey);
+
+
+
+            
+            //Page Set 생성
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+        
+        //createQuestionSet();
+
+
 
 
         // var aaa = document.createElement('div');
