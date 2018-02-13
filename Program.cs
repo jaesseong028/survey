357de50bf6cd4a5c2fcd66e838eac4b7f110b6fc
allyus <allyus@ubcare.co.kr>
2018-02-13 14:09:55 +0900

    Ip 셋팅

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -17,10 +19,26 @@ namespace UBSurvey
             BuildWebHost(args).Run();
         }
 
+        private static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+
+        public static IWebHost BuildWebHost(string[] args) {
+            string myIP = GetLocalIPAddress();
+            return WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseUrls($"http://{myIP}:5000/")
+            .Build();
+        }
     }
 }
