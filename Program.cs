63cf363239db1351f5e4fc08e2b03499b6b5731d
allyus <allyus@ubcare.co.kr>
2018-03-21 13:14:24 +0900

    setting 분기

--- Program.cs
+++ Program.cs
@@ -12,25 +12,54 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using UBSurvey.Common;
 
+
 namespace UBSurvey
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args);
+            var host = BuildWebHost(args);
+            host.Run();
+
+
+            // bool isService = true;
+            // if (Debugger.IsAttached || args.Contains("--console"))
+            // {
+            //     isService = false;
+            // }
+            
+            // if (isService)
+            // {
+            //     host.RunAsService();
+            // }
+            // else
+            // {
+            //     host.Run();
+            // }
         }
 
 
-        public static void BuildWebHost(string[] args) {
-            IWebHost host = WebHost.CreateDefaultBuilder(args)
-                    .UseConfiguration(new ConfigurationBuilder()
-                    .AddJsonFile("hosting.json", optional: true)
+        public static IWebHost BuildWebHost(string[] args) {
+            
+            return WebHost.CreateDefaultBuilder(args)
+                    .ConfigureAppConfiguration(ConfigConfiguration)
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseConfiguration(new ConfigurationBuilder().AddJsonFile("hosting.json", optional: true)
                     .Build()
+                    
             )
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
             .UseStartup<Startup>().Build();
             
-            host.Run();
+        }
+        static void ConfigConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder config)
+        {
+                config.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
         }
     }
 }
